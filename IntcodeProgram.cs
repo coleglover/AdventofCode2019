5a4997fd962ca7e9397feee0dc768acd4badef71
tropicole <cole.r.glover@gmail.com>
2020-10-09 08:43:58 -0700

    Add files via upload

---
+++ IntcodeProgram.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC2019
+{
+    class IntcodeProgram
+    {
+        //int[] intCodeProgramArr = new int[0];
+
+        public IntcodeProgram(int[] aIntcodeProgramArrI)
+        {
+            //int[] intcodeProgramArrF = new int[aIntcodeProgramArrI.Length];
+            int[] intcodeProgramArrF = aIntcodeProgramArrI;
+
+            for (int i = 0; i < intcodeProgramArrF.Length; i = i + 4)
+            {
+
+                Console.WriteLine("\nArray position: "+ i);
+
+                if (intcodeProgramArrF[i] == 1)
+                {
+
+                    int posOfSum = intcodeProgramArrF[i + 3];
+
+                    intcodeProgramArrF[posOfSum] = intcodeProgramArrF[intcodeProgramArrF[i + 1]] + intcodeProgramArrF[intcodeProgramArrF[i + 2]];
+
+                    Console.WriteLine(intcodeProgramArrF[i]);
+                    Console.WriteLine(intcodeProgramArrF[i + 1]);
+                    Console.WriteLine(intcodeProgramArrF[i + 2]);
+                    Console.WriteLine(intcodeProgramArrF[i + 3] + "\n");
+                    Console.WriteLine("new value at position [" + posOfSum + "]" + " = " + intcodeProgramArrF[posOfSum]);
+
+                }
+                else if (intcodeProgramArrF[i] == 2)
+                {
+
+                    int posOfMult = intcodeProgramArrF[i + 3];
+
+                    //assigning value to position of multiplication soltution
+                    intcodeProgramArrF[posOfMult] = intcodeProgramArrF[intcodeProgramArrF[i + 1]] * intcodeProgramArrF[intcodeProgramArrF[i + 2]];
+
+                    Console.WriteLine(intcodeProgramArrF[i]);
+                    Console.WriteLine(intcodeProgramArrF[i + 1]);
+                    Console.WriteLine(intcodeProgramArrF[i + 2]);
+                    Console.WriteLine(intcodeProgramArrF[i + 3] + "\n");
+                    Console.WriteLine("new value at position [" + posOfMult + "]" + " = " + intcodeProgramArrF[posOfMult]);
+
+                }
+                else if (intcodeProgramArrF[i] == 99)
+                {
+                    //Console.WriteLine("HALT - INTCODE: " + intcodeProgramArrF[i] + "\n");
+                    break;
+                }
+            }            
+            Console.WriteLine("intcode value at position [0] is: " + intcodeProgramArrF[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace AoC2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IMPORT MODULE MASS DATA
+            // convert fuel data from string[] -> float[]
+            string[] moduleMassesStr = System.IO.File.ReadAllLines(@"C:\Users\Cole\source\repos\AoC2019\data\adventOfCode2019FuelData.txt");
+            float[] moduleMassesFlt = moduleMassesStr.Select(float.Parse).ToArray();
+            
+            //sending module masses to Fuel Calculator Object
+            FuelCalculator fuelForModulesCalculator = new FuelCalculator(moduleMassesFlt);
+            fuelForModulesCalculator.FuelForFuelCalculator(moduleMassesFlt);
+            
+            //IMPORT INTCODE PROGRAM DATA
+            string intcodeProgramStr = System.IO.File.ReadAllText(@"C:\Users\Cole\source\repos\AoC2019\data\adventOfCode2019IntcodeProgram_working.txt");
+            int[] intcodeProgramIntArr = intcodeProgramStr.Split(',').Select(int.Parse).ToArray();
+            //List<int> intcodeProgramList
+
+            //Console.WriteLine(intcodeProgramIntArr[155]);
+            IntcodeProgram opcodeHandler = new IntcodeProgram(intcodeProgramIntArr);
+
+            //Console.WriteLine(string.Join(",", intcodeProgramIntArr));
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
