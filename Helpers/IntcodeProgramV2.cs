75adefc7cb6a1eab2a7a123fba21ef601aa3b585
tropicole <cole.r.glover@gmail.com>
2020-10-21 11:20:58 -0700

    Completed Day 02

--- Helpers/IntcodeProgramV2.cs
+++ Helpers/IntcodeProgramV2.cs
@@ -14,85 +14,79 @@ namespace AoC2019
         }
         public static void GravityAssistProgram()
         {
-            //bool inputCred = false;
 
             int customInput1 = 12;
             int customInput2 = 2;
 
             int breakValue = 19690720;          
 
-            //while (inputCred == false)
-            //{
+            for (int noun = 0; noun < 100; noun++)
+            {
 
-                for (int noun = 0; noun < 100; noun++)
+                for (int verb = 0; verb < 100; verb++)
                 {
 
-                    for (int verb = 0; verb < 100; verb++)
+
+                    int[] intcodeProgramArr = ImportIntcodeProgram();
+
+                    intcodeProgramArr[1] = noun;
+                    intcodeProgramArr[2] = verb;
+
+                    //Console.WriteLine("~~~NOUN = " + noun);
+                    //Console.WriteLine("~~~~~~~~VERB = " + verb);
+
+
+                    for (int i = 0; i < intcodeProgramArr.Length; i += 4)
                     {
 
 
-                        int[] intcodeProgramArr = ImportIntcodeProgram();
-
-                        intcodeProgramArr[1] = noun;
-                        intcodeProgramArr[2] = verb;
-
-                        //Console.WriteLine("~~~NOUN = " + noun);
-                        //Console.WriteLine("~~~~~~~~VERB = " + verb);
-
-
-                        for (int i = 0; i < intcodeProgramArr.Length; i += 4)
+                        if (intcodeProgramArr[i] == 1)
                         {
 
+                            int posOfSum = intcodeProgramArr[i + 3];
 
-                            if (intcodeProgramArr[i] == 1)
-                            {
+                            intcodeProgramArr[posOfSum] = intcodeProgramArr[intcodeProgramArr[i + 1]] + intcodeProgramArr[intcodeProgramArr[i + 2]];
 
-                                int posOfSum = intcodeProgramArr[i + 3];
-
-                                intcodeProgramArr[posOfSum] = intcodeProgramArr[intcodeProgramArr[i + 1]] + intcodeProgramArr[intcodeProgramArr[i + 2]];
-
-
-                            }
-                            else if (intcodeProgramArr[i] == 2)
-                            {
-
-                                int posOfMult = intcodeProgramArr[i + 3];
-
-                                //assigning value to position of multiplication soltution
-                                intcodeProgramArr[posOfMult] = intcodeProgramArr[intcodeProgramArr[i + 1]] * intcodeProgramArr[intcodeProgramArr[i + 2]];
-
-
-                            }
-                            else if (intcodeProgramArr[i] == 99)
-                            {
-                                //Console.WriteLine("HALT - INTCODE: " + intcodeProgramArr[i] + "\n");
-                                break;
-                            }
-
-                            if (intcodeProgramArr[0] == breakValue)
-                            {
-                                int gravityAssistAns = 100 * intcodeProgramArr[1] + intcodeProgramArr[2];
-
-                                Console.WriteLine("\nSUCCESS! Output value of: " + intcodeProgramArr[0]);
-                                Console.WriteLine("THE NOUN & VERB ARE: " + intcodeProgramArr[1] + " & " + intcodeProgramArr[2] + ", RESPECTIVELY.");
-                                Console.WriteLine("THEREFORE, YOUR ANSWER IS: " + gravityAssistAns);
-
-                                //inputCred = true;
-                                Console.ReadLine();
-                                return;
-                            }
-                        } 
-                        
-                        if (intcodeProgramArr[1] == customInput1 && intcodeProgramArr[2] == customInput2)
-                        {
-                            Console.WriteLine("CUSTOM INPUTS OF: "+ customInput1 + " AND "+ customInput2 + " PRODUCES: " + intcodeProgramArr[0] + " AT POSITION [0]");
-                            Console.ReadLine();
 
                         }
-                        //Console.WriteLine("intcode value at position [0] is: " + intcodeProgramArr[0]);
-                    }                   
-                }
-            //}
+                        else if (intcodeProgramArr[i] == 2)
+                        {
+
+                            int posOfMult = intcodeProgramArr[i + 3];
+
+                            //assigning value to position of multiplication soltution
+                            intcodeProgramArr[posOfMult] = intcodeProgramArr[intcodeProgramArr[i + 1]] * intcodeProgramArr[intcodeProgramArr[i + 2]];
+
+
+                        }
+                        else if (intcodeProgramArr[i] == 99)
+                        {
+                            //Console.WriteLine("HALT - INTCODE: " + intcodeProgramArr[i] + "\n");
+                            break;
+                        }
+
+                        if (intcodeProgramArr[0] == breakValue)
+                        {
+                            int gravityAssistAns = 100 * intcodeProgramArr[1] + intcodeProgramArr[2];
+
+                            Console.WriteLine("\nSUCCESS! Output value of: " + intcodeProgramArr[0]);
+                            Console.WriteLine("THE NOUN & VERB ARE: " + intcodeProgramArr[1] + " & " + intcodeProgramArr[2] + ", RESPECTIVELY.");
+                            Console.WriteLine("THEREFORE, YOUR ANSWER IS: " + gravityAssistAns);
+
+                            Console.ReadLine();
+                            return;
+                        }
+                    } 
+                        
+                    if (intcodeProgramArr[1] == customInput1 && intcodeProgramArr[2] == customInput2)
+                    {
+                        Console.WriteLine("CUSTOM INPUTS OF: "+ customInput1 + " AND "+ customInput2 + " PRODUCES: " + intcodeProgramArr[0] + " AT POSITION [0]");
+                        Console.ReadLine();
+
+                    }
+                    //Console.WriteLine("intcode value at position [0] is: " + intcodeProgramArr[0]);
+                }                   
+            }
         }
     }
 }
