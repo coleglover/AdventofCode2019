99dfb864eab1c601d1f921d05d8e68c3b19b9536
tropicole <cole.r.glover@gmail.com>
2020-10-28 17:01:11 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace AoC2019
     {
         static void Main(string[] args)
         {
-            //IMPORT MODULE MASS DATA
-            // convert fuel data from string[] -> float[]
+            //< IMPORT MODULE MASS DATA
+            //< convert fuel data from string[] -> float[]
             string[] moduleMassesStr = System.IO.File.ReadAllLines(@"C:\Users\Cole\source\repos\AoC2019\data\adventOfCode2019FuelData.txt");
             int[] moduleMassesInt = moduleMassesStr.Select(int.Parse).ToArray();
 
@@ -23,9 +23,17 @@ namespace AoC2019
             //INTCODE PROGRAM
             IntcodeProgram2.GravityAssistProgram();
 
+            //< MANHATTAN DISTANCE
 
+            var wireSteps = ManhattanDistance.ParseWireCoordinates();
 
+            List<(double x, double y)> wireStepsA = wireSteps._wireStepsA;
+            List<(double x, double y)> wireStepsB = wireSteps._wireStepsB;
 
+            int minManhattanDistance = ManhattanDistance.FindManhattanDistance(wireStepsA, wireStepsB);
+            int minWireLengthIntersect = ManhattanDistance.FindMinimumStepsIntersection(wireStepsA, wireStepsB);
+            Console.WriteLine($"the minimum Manhattan distance to a crossed wire is: {minManhattanDistance}");
+            Console.ReadLine();
         }
     }
 }
